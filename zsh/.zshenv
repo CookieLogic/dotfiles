fc8127be9a8453366d66f95370a98ef2cca0998e
Seth Cook <cooker52@gmail.com>
2026-02-22 22:44:04 -0800

    Staged files as of 2026-02-22

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,7 +1,24 @@
-export PATH="${HOME}/bin:${PATH}"
+export DOTFILES_DIR="/Users/${USER}/dotfiles"
+export ZDOTDIR="${DOTFILES_DIR}/zsh"
 
-# Oh My ZSH Settings - https://github.com/ohmyzsh/ohmyzsh/wiki/Settings
-ZSH="${HOME}/.oh-my-zsh"
-ZSH_CUSTOM="${ZDOTDIR}/oh-my-zsh-custom"
-ZSH_THEME="robbyrussell"
-plugins=( git )
+PATH="/Users/${USER}/Library/Python/3.7/bin:$PATH"
+PATH="/Users/${USER}/.cargo/bin:$PATH"
+PATH="/Users/${USER}/bin:$PATH"
+PATH="/Users/${USER}/Library/Python/3.7/bin:$PATH"
+export PATH="$PATH"
+
+export PYTHONPATH="/Users/${USER}/Library/Python/3.7/lib"
+
+#export ZSH="/Users/${USER}/.oh-my-zsh"
+export HOMEBREW_NO_ENV_HINTS=1  # Hide Brew hints
+export LESS="-RS"
+
+# Default Programs
+export EDITOR=vim
+export PAGER=less
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/${USER}/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/cookie/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/${USER}/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/cookie/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
--- zsh/setup-install.sh
+++ zsh/setup-install.sh
@@ -1,21 +1,50 @@
 #/bin/bash
 
 DOTFILES_DIR="${1:-${HOME}/.dotfiles}"
-ZSHENV_FILE="${HOME}/.zshenv"
+ZDOTFILES_DIR="${DOTFILES_DIR}/zsh"
+ZSH_FILES=(
+  .zshenv
+)
 
-if [ ! -d "$DOTFILES_DIR" ] ; then
-  echo "ERROR: ZSH does not exist in the dotfiles dir, path=${DOTFILES_DIR}"
+if [ ! -d "$ZDOTFILES_DIR" ] ; then
+  echo "ERROR: ZSH does not exist in the dotfiles dir, path=${ZDOTFILES_DIR}"
   exit 1
 fi
 
-if [ -f "$ZSHENV_FILE" ] ; then
-  move "$ZSHENV_FILE"
+#if [ -f "$ZSH_ENV_FILE" ] ; then
+#  move "$ZSH_ENV_FILE"
+#fi
+
+#cat - >"$ZSH_ENV_FILE" <<EOF
+#DOTFILES_DIR="${DOTFILES_DIR}"
+#ZDOTDIR="\${DOTFILES_DIR}/zsh"
+#source "\${ZDOTDIR}/.zshenv"
+#EOF
+
+for filename in "${ZSH_FILES[@]}" ; do
+  zfilename="${HOME}/${filename}"
+  if [ -e "${zfilename}" ] ; then
+    read -p "${zfilename} already exists, overwrite? [y/n] "
+    if [[ "$REPLY" =~ ^[yY]$ ]] ; then
+      echo "Backing up and replacing ${zfilename}."
+      mv "$zfilename" "$zfilename.$(date +%Y-%m-%dT%H:%M:%S).bak"
+    else
+      continue
+    fi
+  elif [ -L "$zfilename" ] ; then
+    read -p "${zfilename} is linked to another file, overwrite? [y/n] "
+    if [[ "$REPLY" =~ ^[yY]$ ]] ; then
+      echo "Removing soft link at ${zfilename}."
+      rm -f "$zfilename"
+    else
+      continue
+    fi
+  fi
+
+  echo "Linking ${zfilename} to ${ZDOTFILES_DIR}/${filename}"
+  ln -s "${ZDOTFILES_DIR}/${filename}" "$zfilename"
+done
+
+if [ ! -d "${HOME}/.oh-my-zsh" ] ; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-cat - >"$ZSHENV_FILE" <<EOF
-DOTFILES_DIR="${DOTFILES_DIR}"
-ZDOTDIR="\${DOTFILES_DIR}/zsh"
-source "\${ZDOTDIR}/.zshenv"
-EOF
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
